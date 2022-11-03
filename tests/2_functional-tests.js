4e10d6e6574d7d8a688167d6c19cf454194bb0bb
Katherine Kent <104234473+k4tiek3nt@users.noreply.github.com>
2022-11-03 17:22:28 -0600

    Initial commit

---
+++ tests/2_functional-tests.js
@@ -0,0 +1,82 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const server = require('../server');
+
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+suite('Functional Tests', function () {
+  this.timeout(5000);
+  suite('Integration tests with chai-http', function () {
+    // #1
+    test('Test GET /hello with no name', function (done) {
+      chai
+        .request(server)
+        .get('/hello')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
+          done();
+        });
+    });
+    // #2
+    test('Test GET /hello with your name', function (done) {
+      chai
+        .request(server)
+        .get('/hello?name=xy_z')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
+          done();
+        });
+    });
+    // #3
+    test('Send {surname: "Colombo"}', function (done) {
+      chai
+        .request(server)
+        .put('/travellers')
+
+        .end(function (err, res) {
+          assert.equal();
+
+          done();
+        });
+    });
+    // #4
+    test('Send {surname: "da Verrazzano"}', function (done) {
+      assert.equal();
+
+      done();
+    });
+  });
+});
+
+const Browser = require('zombie');
+
+suite('Functional Tests with Zombie.js', function () {
+  this.timeout(5000);
+
+
+
+  suite('Headless browser', function () {
+    test('should have a working "site" property', function() {
+      assert.isNull(Browser.site);
+    });
+  });
+
+  suite('"Famous Italian Explorers" form', function () {
+    // #5
+    test('Submit the surname "Colombo" in the HTML form', function (done) {
+      assert.equal();
+
+      done();
+    });
+    // #6
+    test('Submit the surname "Vespucci" in the HTML form', function (done) {
+      assert.equal();
+
+      done();
+    });
+  });
+});
