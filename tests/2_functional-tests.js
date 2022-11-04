3242ac4ac7c9bd708c89e1d431814e8e4cb57422
Katherine Kent <104234473+k4tiek3nt@users.noreply.github.com>
2022-11-04 17:40:32 -0600

    updated functional test with Zombie.js
    Functional Tests with Zombie.js
        Headless browser
          ✔ should have a working "site" property
        "Famous Italian Explorers" form
          ✔ Submit the surname "Colombo" in the HTML form (498ms)
          ✔ Submit the surname "Vespucci" in the HTML form (423ms)
    
    All 25/25 tests passing - finally corrected AssertionError No open window with HTML with last update (Submit the surname "Colombo")

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -79,7 +79,7 @@ suite('Functional Tests with Zombie.js', function () {
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
+      assert.isNotNull(Browser.site);
     });
   });
 
@@ -99,9 +99,15 @@ suite('Functional Tests with Zombie.js', function () {
 
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.equal();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        })
+      })
     });
   });
 });
