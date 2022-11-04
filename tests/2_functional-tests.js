41e67a921e9b3f4cc131be73d483cdf5e934c71e
Katherine Kent <104234473+k4tiek3nt@users.noreply.github.com>
2022-11-04 17:23:28 -0600

    updated functional test with Zombie.js
    Functional Tests with Zombie.js
        Headless browser
          ✔ should have a working "site" property
        "Famous Italian Explorers" form
          ✔ Submit the surname "Colombo" in the HTML form (532ms)

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -66,31 +66,37 @@ suite('Functional Tests', function () {
   });
 });
 
-const Browser = require('zombie');
-
-const browser = new Browser();
-
-Browser.site = 'https://mochachai-testing.k4tiek3nt.repl.co';
+const Browser = require('zombie'); 
+Browser.site = "https://boilerplate-mochachai.k4tiek3nt.repl.co";
 
 suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
+  const browser = new Browser();
   
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  })
+
+
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
-      assert.isNotNull(Browser.site);
-      suiteSetup(function(done) {
-    return browser.visit('/', done);
-  });
+      assert.isNotNull(browser.site);
     });
   });
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.equal();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        })
+      })
     });
+
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
       assert.equal();
